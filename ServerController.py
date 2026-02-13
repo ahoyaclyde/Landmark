950ad42e1e40bb528e0750fcd0818e05ece7ee82
Libera8 <clydensoo@gmail.com>
2026-02-13 16:41:17 +0300

    PkgUtil Issue

--- ServerController.py
+++ ServerController.py
@@ -1,4 +1,4 @@
-import asyncio
+]import asyncio
 from flask import Flask , url_for , render_template , request , redirect , send_from_directory
 from jinja2 import Environment , FileSystemLoader
 from flask.views import View
@@ -34,23 +34,60 @@ CompanyID = "PointCare"
 
 
 
-class Company_Display_Profile(View):
-   methods = ['GET']  
-   def dispatch_request(self) -> str :
-        CompanyID = "DensoTechnologies"
-        if request.method == 'GET':
-            return render_template('Company_Display_Profile.html' , CompanyID = CompanyID  )
+# Subscriber Model Onboarding 
+# Intergrates Clients To Customer Base List  
+# Should Have A Db Impl of Storing  Reg - Subscribers 
+class Onboarding_Platform(View): 
+    methods = ['GET', 'POST']  
+
+    def dispatch_request(self) -> str :
+      
+    
+        if request.method == 'POST':
+            EmailAddress = request.form.get("Email")
+            Phone = request.form.get("Contact")
+            Subject = """ Thank you for joining Osefa homes & Developers . You registered with phone [ {0} ] . Kindly usse that to log in """.format(Phone)
+            Body = """ Welcome to Osefa Homes & Devs  """ 
+          
+            base.Create_Email(EmailAddress , Subject , Body)
+            return redirect(url_for('Home' , CompanyID = CompanyID , Info = Info  ))
+
         else: 
             # Return requested profile thru client connect 
-            return render_template('Company_Display_Profile.html' , CompanyID = CompanyID  )
+            return redirect(url_for('Home' , CompanyID = CompanyID , Info = Info  , ))
+
+
+
+
+# Contact Us Master Form Submission Protocol
+# Relays Feedback Thru Email After Supply Of Neck Creds
+
+
+
+
+# Base Route Address For Campaign/Landing Page Scenarios 
+# Initial Page 
+class Dash_Page_Context(View):
+   methods = ['GET']  
+   def Render_Year(self):
+        Current_Year = str(2025)
+        return Current_Year
+
+   def dispatch_request(self) -> str :
+        
+        YEAR = self.Render_Year()
+        if request.method == 'GET':
+            return render_template("Company_Display_Profile.html" , CompanyID = CompanyID  , YEAR = YEAR  )
+        else: 
+            # Return requested profile thru client connect 
+            return render_template("Company_Display_Profile.html" , CompanyID = CompanyID , YEAR = YEAR   )
         
 
 
 
-
-
-app.add_url_rule('/', view_func=Company_Display_Profile.as_view('Home'))
-
+# Registering App Urls Routes 
+# (1 - Campaign Route Company Portal )
+app.add_url_rule('/', view_func=Dash_Page_Context.as_view('Home'))
 
 
 if __name__=='__main__':
